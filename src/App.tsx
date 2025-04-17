f22a44026792299bd9d806eefa8432eeb464a160
Tedd Lee <tedd0328@gmail.com>
2025-04-16 22:22:36 -0400

    test local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
+  const [words, setWords] = useState<string[]>([]);
+
+  useEffect(() => {
+    const listener = () => {
+      chrome.storage.sync.get(["words"], (result) => {
+        setWords(result.words);
+      })
+    };
+    chrome.storage.onChanged.addListener(listener);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      chrome.storage.onChanged.removeListener(listener);
+    };
+  }, []);
+
   return (
     <>
       <h1>Saved Chinese Words</h1>
-      <Card word={'你好'} />
+      {words.map((word, index) => (
+          <Card key={index} word={word} />
+      ))}
     </>
   )
 }
